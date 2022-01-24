a363315042ace65989305c8917e38e5db49c5c86
Gilvan Gomes <42100457+GilvanG@users.noreply.github.com>
2022-01-24 13:20:26 -0300

    Change replaceAll for replace

--- routes/get.js
+++ routes/get.js
@@ -14,8 +14,8 @@ router.get('/:url', async (req, res, next) => {
 	res.set('Access-Control-Allow-Origin', '*');
 	var url = req.params.url;
 	console.log(url)
-	url = url.replace(/../g, '/');
-    	console.log(url)
+	url = url.split('..').join('/');
+	console.log(url)
 	const response = await axios.get(url)//https://www.fruityvice.com/api/fruit/all");
         res.send(response.data);
     }catch (err) {
